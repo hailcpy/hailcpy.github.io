f28f4ccf391c5bb4077b8e9c22cc4709079b5bbf
hailcpy <60291306+hailcpy@users.noreply.github.com>
2020-06-12 14:33:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
-
-
 var i;
 var j;
-i=0;
-j=1;
+i = Math.floor(Math.random()*120)
+j = Math.floor(Math.random()*120);
+$(".image1").attr("src", 'animals/stim'+i+'.png');
+$(".image2").attr("src", 'animals/stim'+j+'.png');
 
 function uncheck(){
     var k =1
     for(k = 1;k<9;k++){
         $('#'+k).prop("value","2.5");
+        $('#rangetext'+k).text(rangeValues[$('#'+k).val()]);
     }
 };
+var rangeValues =
+{
+    "1": "very dissimilar",
+    "2": "dissimilar",
+    "3": "no idea",
+    "4": "similar",
+    "5": "very similar",
+};
+
+$('#1').on('input change' ,function (){
+    $('#rangetext1').text(rangeValues[$(this).val()]);
+});
+$('#2').on('input change' ,function (){
+    $('#rangetext2').text(rangeValues[$(this).val()]);
+});
+$('#3').on('input change' ,function (){
+    $('#rangetext3').text(rangeValues[$(this).val()]);
+});
+$('#4').on('input change' ,function (){
+    $('#rangetext4').text(rangeValues[$(this).val()]);
+});
+$('#5').on('input change' ,function (){
+    $('#rangetext5').text(rangeValues[$(this).val()]);
+});
+$('#6').on('input change' ,function (){
+    $('#rangetext6').text(rangeValues[$(this).val()]);
+});
+$('#7').on('input change' ,function (){
+    $('#rangetext7').text(rangeValues[$(this).val()]);
+});
+$('#8').on('input change' ,function (){
+    $('#rangetext8').text(rangeValues[$(this).val()]);
+});
 
 $("#foo").submit(function(event){
     var $form = $(this);
@@ -21,8 +55,6 @@ $("#foo").submit(function(event){
     i = Math.floor(Math.random()*120)
     j = Math.floor(Math.random()*120);
     }
-    console.log(i);
-    console.log(j);
     uncheck();
     $(".image1").attr("src","");
     $(".image2").attr("src","");
@@ -70,13 +102,6 @@ function submitform($form){
 $(".next2").click(function(){
     var $form = $("#foo");
     submitform($form);
-    // $("body").css("background-image", "url(" + "back.jpg" + ")")
     $("body").replaceWith("<p style  = 'text-align:center; font-size: 50px;'>Thank you for taking part in the experiment</p>");
     
 })
-
-// Variable to hold request
-
-
-// Bind to the submit event of our form
-
