7088379f4ae6ce5ab30a2d5e926c24eff473675f
Sanket Agrawal <babasanku@users.noreply.github.com>
2020-06-24 12:11:55 +0530

    Instructions page
    Added instructions page
    Changed the layout of welcome page
    Removed style.css
    Added style tags in respective html pages.

--- main.js
+++ main.js
@@ -11,73 +11,90 @@ function uncheck(){
         $('#'+k).prop("value","5");
 		$('#'+k).tooltip('hide');
 		$('#checkbox'+k).prop("checked", false);
+		$('#rangetext'+k).text("Slide");
     }
 };
 
 
 $('#1').on('input change' ,function (){
 	$(this).tooltip('show');
+	$('#rangetext1').text("");
 	$('#checkbox1').prop("checked", false);
 });
 $('#2').on('input change' ,function (){
 	$(this).tooltip('show');
+	$('#rangetext2').text("");
 	$('#checkbox2').prop("checked", false);
 });
 $('#3').on('input change' ,function (){
 	$(this).tooltip('show');
+	$('#rangetext3').text("");
 	$('#checkbox3').prop("checked", false);
 });
 $('#4').on('input change' ,function (){
 	$(this).tooltip('show');
+	$('#rangetext4').text("");
 	$('#checkbox4').prop("checked", false);
 });
 $('#5').on('input change' ,function (){
 	$(this).tooltip('show');
+	$('#rangetext5').text("");
 	$('#checkbox15').prop("checked", false);
 });
 $('#6').on('input change' ,function (){
 	$(this).tooltip('show');
+	$('#rangetext6').text("");
 	$('#checkbox6').prop("checked", false);
 });
 $('#7').on('input change' ,function (){
 	$(this).tooltip('show');
+	$('#rangetext7').text("");
 	$('#checkbox7').prop("checked", false);
 });
 $('#8').on('input change' ,function (){
 	$(this).tooltip('show');
+	$('#rangetext8').text("");
 	$('#checkbox8').prop("checked", false);
 });
 
 $('#checkbox1').change(function (){
 	$('#1').prop("value", "5");
+	$('#rangetext1').text("Slide");
 	$('#1').tooltip('hide');
 });
 $('#checkbox2').change(function (){
 	$('#2').prop("value", "5");
+	$('#rangetext2').text("Slide");
 	$('#2').tooltip('hide');
 });
 $('#checkbox3').change(function (){
 	$('#3').prop("value", "5");
+	$('#rangetext3').text("Slide");
 	$('#3').tooltip('hide');
 });
 $('#checkbox4').change(function (){
 	$('#4').prop("value", "5");
+	$('#rangetext4').text("Slide");
 	$('#4').tooltip('hide');
 });
 $('#checkbox5').change(function (){
 	$('#5').prop("value", "5");
+	$('#rangetext5').text("Slide");
 	$('#5').tooltip('hide');
 });
 $('#checkbox6').change(function (){
 	$('#6').prop("value", "5");
+	$('#rangetext6').text("Slide");
 	$('#6').tooltip('hide');
 });
 $('#checkbox7').change(function (){
 	$('#7').prop("value", "5");
+	$('#rangetext7').text("Slide");
 	$('#7').tooltip('hide');
 });
 $('#checkbox8').change(function (){
 	$('#8').prop("value", "5");
+	$('#rangetext8').text("Slide");
 	$('#8').tooltip('hide');
 });
 
@@ -106,7 +123,7 @@ function submitform($form){
     // setup some local variables
     console.log($form)
     // Let's select and cache all the fields
-    var $inputs = $form.find("input, select, button, textarea");
+    var $inputs = $form.find("input, select, button, textarea, range, checkbox");
     
     var serializedData = 'image1='+i+'&';
     serializedData += 'image2='+j+'&';
